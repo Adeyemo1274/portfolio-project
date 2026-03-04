010279bf742a0ddca4281ed8966272f0e6de8752
Adeyemo1274 <tcash1274@gmail.com>
2026-03-04 23:52:47 +0100

    portfolio-project

---
+++ src/script/data.js
@@ -0,0 +1,10 @@
+const projects = [
+  {
+    id: 1,
+    name: "Admin Dashboard",
+    image: "src/assets/admin.jpg",
+    description: "A real-time sneaker store",
+    liveLink: "https://adeyemo1274.github.io/admin-dashboard/"
+  }
+  
+];
