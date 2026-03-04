33307f06a1f7dae5be64148dad6bbcc075a1499e
Adeyemo1274 <tcash1274@gmail.com>
2026-03-05 00:43:56 +0100

    portfolio-project

--- src/script/data.js
+++ src/script/data.js
@@ -5,6 +5,21 @@ const projects = [
     image: "src/assets/admin.jpg",
     description: "A real-time sneaker store",
     liveLink: "https://adeyemo1274.github.io/admin-dashboard/"
-  }
+  },
+
+   {
+    id: 2,
+    name: "Landing Page",
+    image: "src/assets/landing.jpg",
+    description: "A shortlet landing page",
+    liveLink: "https://adeyemo1274.github.io/landing-page/"
+  },
   
+    {
+    id: 3,
+    name: "Simple Calculator",
+    image: "src/assets/calculator.jpg",
+    description: "A basic calculator",
+    liveLink: "https://adeyemo1274.github.io/simple-calculator/"
+  }
 ];
